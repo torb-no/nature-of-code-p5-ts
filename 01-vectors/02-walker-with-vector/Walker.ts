12cb1d9baba34fbbfc4fd970524f7d32f391f433
Torbjørn Vik Lunde <torbjornlunde@users.noreply.github.com>
2018-10-07 10:30:26 +0200

    Exercise 1.2

---
+++ 01-vectors/02-walker-with-vector/Walker.ts
@@ -0,0 +1,28 @@
+class Walker {
+	pos: p5.Vector;
+
+	constructor() {
+		this.pos = new p5.Vector(250, 250);
+	}
+
+	step() {
+		// If walker is too close the mouse tend towards
+		// the oposite direction
+		if (p.dist(this.pos.x, this.pos.y, p.mouseX, p.mouseY) < 50) {
+			if (this.pos.x < p.mouseX) this.pos.x += p.random(-2, 1);
+			else this.pos.x += p.random(-1, 2);
+
+			if (this.pos.y < p.mouseY) this.pos.y += p.random(-2, 1);
+			else this.pos.y += p.random(-1, 2);
+		}
+		else {
+			this.pos.x += p.random(-1, 1);
+			this.pos.y += p.random(-1, 1);
+		}
+	}
+
+	display() {
+		p.fill(255, 255, 255);
+		p.point(this.pos.x, this.pos.y);
+	}
+}
